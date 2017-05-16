0ab191d3af1b71b4a2130e916bade5be73d65d56
Abel <sdwyub@qq.com>
2017-05-16 14:39:01 +0800

    版本重构

--- new_stock/src/js/regist/index.js
+++ new_stock/src/js/regist/index.js
@@ -1,10 +1,4 @@
 
-
-/*App.registView("start",new App.View({
-  el:"#start",
-  model:new App.Model()
-}),true);*/
-
 App.registView("list",new App.View({
   el:"#list",
   model:new App.Model()
@@ -14,5 +8,3 @@ App.registView("detail",new App.View({
   el:"#detail",
   model:new App.Model()
 }),false);
-
-      
--- new_stock/src/js/view/detail.js
+++ new_stock/src/js/view/detail.js
@@ -1,44 +1,19 @@
-;App.detailView.wrap({
 
-    afterRender: function (arg) {
-        window.scrollTo(0, 0);
-        if (arg && App.listView.notIList) {
-            var list = App.listView.notIList;
-            var resultObj = {};
-            for (var i = 0; i < list.length; i++) {
-                if (list[i].stock_code == arg) {
-                    resultObj = list[i];
-                    break;
-                }
-            }
-            this.model.set('allocation_date', resultObj.allocation_date ? resultObj.allocation_date : "--");
-            this.model.set('prod_name', resultObj.prod_name);
-            this.model.set('stock_code', resultObj.stock_code);
-            this.model.set('prod_code', resultObj.prod_code);
-            this.model.set('issue_price', resultObj.issue_price ? (parseFloat(resultObj.issue_price)).toFixed(2) : '--');
-            /*this.model.set('naps',resultObj.naps?resultObj.naps:'--');*/
-            this.model.set('secu_market', resultObj.secu_market == 83 ? '上海证券交易所' : '深圳证券交易所');
-            this.model.set('allot_max', resultObj.allot_max ? (parseFloat(resultObj.allot_max) / 10000).toFixed(2) : '--');
-            this.model.set('issue_amount', resultObj.issue_amount ? (parseFloat(resultObj.issue_amount) / 10000).toFixed(2) : '--');
-            this.model.set('distribution_date', resultObj.distribution_date ? resultObj.distribution_date : "--");
-            this.model.set('indurstry', resultObj.indurstry ? resultObj.indurstry : "--");
-            this.model.set('diluted_pe_ratio', resultObj.diluted_pe_ratio ? parseFloat(resultObj.diluted_pe_ratio).toFixed(2) : '--');
-            this.model.set('issue_vol', resultObj.issue_vol ? (parseFloat(resultObj.issue_vol) / 10000).toFixed(2) : '--');
-            this.$el.find(".gsjj").html(resultObj.brief_introduction);
-
-        }
-        else {
-            window.location.href = "#list";
-        }
-
+/**
+ * VIEW detail
+ * @param  {[type]} $ [description]
+ * @return {[type]}   [description]
+ */
+;(function(){
+  App.detailView.wrap({
+    beforeRender:function(){
+      //TODO
+      return true;
+    },
+    ready:function(){
+      //TODO
+    },
+    afterUnRender:function(){
     }
-
-    /* eventBind:function(){
-     this.$el.find(".top").bind("tap",function(){
-
-     })
-     },
-     */
-
-
-})
+  });
+})();
