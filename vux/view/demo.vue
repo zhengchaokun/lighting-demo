2f5fde5783b71d0e2a52e08a57c20d60cc2368d7
Abel <sdwyub@qq.com>
2017-06-29 16:20:16 +0800

    增加vux实例

---
+++ vux/view/demo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <tip>I am a tip.</tip>
+
+        <tip align="center">I am a tip align center.</tip>
+    </div>
+</template>
+
+<script>
+    import Tip from 'vux/src/components/tip/index.vue'
+    export default {
+        components: {
+            Tip
+        }
+    }
+</script>
