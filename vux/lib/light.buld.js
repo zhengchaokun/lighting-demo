6345341b36c17abef4bf4c8feb14e41b64b6188d
Abel <sdwyub@qq.com>
2017-07-05 09:38:03 +0800

    vux集成功能优化

--- vux/lib/light.buld.js
+++ vux/lib/light.buld.js
@@ -1,3 +1,14 @@
-module.exports = function (config) {
+module.exports = function (light,config) {
+    const vuxLoader = light.require('vux-loader');
+    config = vuxLoader.merge(config, {
+        options: {
+            projectRoot:`${__dirname}/lib`
+        },
+        plugins:  [
+            {
+                name: 'vux-ui'
+            }
+        ]
+    })
     return config
 }
