7bde0a5bf8f26802bc21d50d2054d514110d9378
Abel <sdwyub@qq.com>
2017-07-05 09:28:37 +0800

    vux集成功能优化

--- vux/app.js
+++ vux/app.js
@@ -1,4 +1,55 @@
 import App from "light"
+import Vuex from 'vuex'
+import vuexI18n from 'vuex-i18n'
+
+let Vue = App.Vue;
+Vue.use(Vuex)
+/**
+ * you can add your module here
+ */
+let store = new Vuex.Store({
+    modules: {
+        i18n: vuexI18n.store
+    }
+})
+
+store.registerModule('vux', {
+    state: {
+        demoScrollTop: 0,
+        isLoading: false,
+        direction: 'forward'
+    },
+    mutations: {
+        updateDemoPosition (state, payload) {
+            state.demoScrollTop = payload.top
+        },
+        updateLoadingStatus (state, payload) {
+            state.isLoading = payload.isLoading
+        },
+        updateDirection (state, payload) {
+            state.direction = payload.direction
+        }
+    },
+    actions: {
+        updateDemoPosition ({commit}, top) {
+            commit({type: 'updateDemoPosition', top: top})
+        }
+    }
+})
+
+Vue.use(vuexI18n.plugin, store)
+
+// plugins
+import { LocalePlugin, DevicePlugin, ToastPlugin, AlertPlugin, ConfirmPlugin, LoadingPlugin, WechatPlugin, AjaxPlugin, AppPlugin } from 'vux'
+Vue.use(DevicePlugin)
+Vue.use(ToastPlugin)
+Vue.use(AlertPlugin)
+Vue.use(ConfirmPlugin)
+Vue.use(LoadingPlugin)
+Vue.use(WechatPlugin)
+Vue.use(AjaxPlugin)
+Vue.use(LocalePlugin)
+
 
 App.filter("start",function (next) {
     //启动拦截器
--- vux/view/demo.vue
+++ vux/view/demo.vue
@@ -1,16 +1,85 @@
 <template>
     <div>
-        <tip>I am a tip.</tip>
+        <group>
+            <x-switch :title="$t('Show Me')" v-model="show"></x-switch>
+        </group>
+        <div v-transfer-dom>
+            <alert v-model="show" :title="$t('Congratulations')" @on-show="onShow" @on-hide="onHide"> {{ $t('Your Message is sent successfully~') }}</alert>
+        </div>
 
-        <tip align="center">I am a tip align center.</tip>
+        <group title="prop:content">
+            <x-switch :title="$t('Show Me')" v-model="show2"></x-switch>
+        </group>
+        <div v-transfer-dom>
+            <alert v-model="show2" :title="$t('Congratulations')" :content="$t('Your Message is sent successfully~')"></alert>
+        </div>
+
+        <group :title="$t('Use as a plugin')">
+            <cell :title="$t('Show Me')" @click.native="showPlugin" is-link></cell>
+            <cell :title="$t('will auto close in 3s')" @click.native="showPluginAuto" is-link></cell>
+        </group>
     </div>
 </template>
 
+<i18n>
+    Show Me:
+    zh-CN: 显示
+    Use as a plugin:
+    zh-CN: 以插件方式调用
+    Congratulations:
+    zh-CN: 恭喜
+    Your Message is sent successfully~:
+    zh-CN: 消息已成功发送
+    Do you agree?:
+    zh-CN: 同意不?
+    will auto close in 3s:
+    zh-CN: 3秒后关闭
+</i18n>
+
 <script>
-    import Tip from 'vux/src/components/tip/index.vue'
+    import { Alert, Group, XSwitch, Cell, TransferDomDirective as TransferDom } from 'vux'
     export default {
+        directives: {
+            TransferDom
+        },
         components: {
-            Tip
+            Alert,
+            Group,
+            XSwitch,
+            Cell
+        },
+        data () {
+            return {
+                show: false,
+                show1: false,
+                show2: false
+            }
+        },
+        methods: {
+            onHide () {
+                console.log('on hide')
+            },
+            onShow () {
+                console.log('on show')
+            },
+            showPlugin () {
+                this.$vux.alert.show({
+                    title: 'VUX is Cool',
+                    content: this.$t('Do you agree?'),
+                    onShow () {
+                        console.log('Plugin: I\'m showing')
+                    },
+                    onHide () {
+                        console.log('Plugin: I\'m hiding now')
+                    }
+                })
+            },
+            showPluginAuto () {
+                this.showPlugin()
+                setTimeout(() => {
+                    this.$vux.alert.hide()
+                }, 3000)
+            }
         }
     }
 </script>
