22aae1629cc60208ee333a7110d72bec571ddc9d
Abel <sdwyub@qq.com>
2017-08-28 08:55:47 +0800

    增加异步代码

--- todolist/app.js
+++ todolist/app.js
@@ -1,5 +1,11 @@
 import App from "light"
 
+const asyn = import("./lib/asyn.js");
+
+asyn.then(function (m) {
+    m.test();
+})
+
 App.filter("start",function (next) {
     //启动拦截器
     App.log("启动拦截器");
