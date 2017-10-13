8ae0d2073f50063be0c95b47626f54c6ba840a27
unknown <chenljy19172@hs.handsome.com.cn>
2017-10-13 15:18:29 +0800

    add

---
+++ ligui/view/demo.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div>
+       <el-button>按钮</el-button>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {}
+        }
+    }
+</script>
+<style lang="less">
+
+</style>
