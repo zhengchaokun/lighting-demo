d1a24628b2717d1a2409e89d166313119df8a0ed
Abel <sdwyub@qq.com>
2017-11-13 15:12:53 +0800

    梳理demo的目录结构

--- jsnative/view/light/index2.vue
+++ jsnative/view/light/index2.vue
@@ -1,59 +1,100 @@
 <template>
-    <list class="list" @loadmore="fetch" loadmoreoffset="10">
-        <cell class="cell" v-for="num in lists">
-            <div class="panel">
-                <text class="text">{{num}}</text>
-            </div>
-        </cell>
-    </list>
+    <div style="padding: 5px;">
+        <text class="result">{{result}}</text>
+        <div class="row">
+            <text class="btn" @click="input(1)">1</text>
+            <text class="btn" @click="input(2)">2</text>
+            <text class="btn" @click="input(3)">3</text>
+            <text class="btn btn-operator" @click="input('+')">+</text>
+        </div>
+        <div class="row">
+            <text class="btn" @click="input(4)">4</text>
+            <text class="btn" @click="input(5)">5</text>
+            <text class="btn" @click="input(6)">6</text>
+            <text class="btn btn-operator" @click="input('-')">-</text>
+        </div>
+        <div class="row">
+            <text class="btn" @click="input(7)">7</text>
+            <text class="btn" @click="input(8)">8</text>
+            <text class="btn" @click="input(9)">9</text>
+            <text class="btn btn-operator" @click="input('*')">*</text>
+        </div>
+        <div class="row">
+            <text class="btn" @click="input(0)">0</text>
+            <text class="btn" @click="input('.')">.</text>
+            <text class="btn" @click="clear('AC')" style="font-size: 25px">AC</text>
+            <text class="btn btn-operator" @click="calculate">=</text>
+        </div>
+    </div>
 </template>
 
+<style scoped>
+    .row {
+        flex-direction: row;
+    }
+    .result {
+        text-align: right;
+        background-color: #666;
+        font-size: 40px;
+        color: white;
+    }
+    .btn {
+        flex: 1;
+        text-align: center;
+        background-color: #eee;
+        font-size: 36px;
+    }
+    .btn, .result {
+        height: 100px;
+        padding: 30px;
+        margin: 5px;
+    }
+    .btn-operator {
+        background-color: #669;
+        font-size: 40px;
+        color: white;
+    }
+</style>
+
 <script>
-
-    const LOADMORE_COUNT = 4
-
-    export default {
-        data () {
+    var OP = ['+', '-', '*', '/'];
+    module.exports = {
+        data: function () {
             return {
-                lists: [1, 2, 3, 4, 5]
+                result: '',
+                inputs: []
             }
         },
         methods: {
-            fetch (event) {
-                const modal = weex.requireModule('modal')
-                modal.toast({ message: 'loadmore', duration: 1 })
-
-                setTimeout(() => {
-                    const length = this.lists.length
-                    for (let i = length; i < length + LOADMORE_COUNT; ++i) {
-                    this.lists.push(i + 1)
+            input: function(value) {
+                var modal = weex.requireModule('modal')
+                modal.toast({ message: 'input: ' + value, duration: 1 })
+//                var value = e.target.attr['value'];
+                var inputs = this.inputs;
+                var lastOne = inputs.length ? inputs[inputs.length - 1] : '';
+                if (OP.indexOf(lastOne) > -1 && OP.indexOf(value) > -1) {
+                    return;
                 }
-            }, 800)
+                inputs.push(value);
+                var buf = [], char;
+                for (var i = 0; i < inputs.length; i++) {
+                    char = inputs[i];
+                    if (OP.indexOf(char) > -1) {
+                        char = ' ' + char + ' ';
+                    }
+                    buf.push(char);
+                }
+                this.result = buf.join('');
+            },
+            calculate: function() {
+                var result = eval(this.result);
+                this.inputs = [result];
+                this.result = result;
+            },
+            clear: function() {
+                this.inputs = [];
+                this.result = '';
             }
         }
     }
 </script>
-
-<style scoped>
-    .list{
-        height: 1000px;
-    }
-    .panel {
-        width: 600px;
-        height: 250px;
-        margin-left: 75px;
-        margin-top: 35px;
-        margin-bottom: 35px;
-        flex-direction: column;
-        justify-content: center;
-        border-width: 2px;
-        border-style: solid;
-        border-color: rgb(162, 217, 192);
-        background-color: rgba(162, 217, 192, 0.2);
-    }
-    .text {
-        font-size: 50px;
-        text-align: center;
-        color: #41B883;
-    }
-</style>
