fc01a0e076375744f3c1cb2b1f4a2a0934be6297
ruanzhen18683 <253840533@qq.com>
2017-11-21 14:14:15 +0800

    update-c

---
+++ jsnative/view/template/d.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <text>
+       Hello,World !
+    </text>
+</template>
+<script>
+    export default {
+        data(){
+            return {}
+        }
+    }
+</script>
+<style scoped src="../../css/ui.css"></style>
+<style scoped>
+
+</style>
