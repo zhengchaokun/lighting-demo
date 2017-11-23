e47a92a2c1aba69f96432b5d02d1dc4cf35fc008
ruanzhen18683 <253840533@qq.com>
2017-11-23 15:32:50 +0800

    update-a

--- jsnative/view/template/a.vue
+++ jsnative/view/template/a.vue
@@ -4,36 +4,39 @@
             <text class="header-title">今日头条</text>
             <input class="header-input" type="text" placeholder="砖石是个好东西啊|你说呢"/>
         </div>
-        <div class="top-nav flex-row border-bottom">
-            <text class="top-nav-item row-item">热点</text>
-            <text class="top-nav-item row-item">热点</text>
-            <text class="top-nav-item row-item" style="color:#f1a7a9">热点</text>
-            <text class="top-nav-item row-item">热点</text>
-            <text class="top-nav-item row-item">热点</text>
-        </div>
-        <div class="news">
-            <list style="height: 1000px">
-                <cell class=""  v-for="data in news">
-                    <div class="news-item border-bottom">
-                        <text class="news-item-title">李嘉诚名下尝试集团出售香港中环中，成交金额402港元...</text>
-                        <div class="news-item-pic-wrap flex-row">
-                            <image class="news-item-pic row-item"></image>
-                            <image class="news-item-pic row-item"></image>
-                            <image class="news-item-pic row-item"></image>
-                        </div>
-                        <div class="news-item-append flex-row">
-                            <div class="row-item flex-row">
-                                <text class="row-item news-item-append-text">虎嗅网</text>
-                                <text class="row-item news-item-append-text">11评论</text>
-                                <text class="row-item news-item-append-text">20分钟前</text>
-                            </div>
-                            <div class="row-item"></div>
-                        </div>
-                    </div>
-                </cell>
-            </list>
-            <div style="height: 200px;"></div>
-        </div>
+        <scroller class="scroller" scrollDirection="horizontal" show-scrollbar="false" >
+            <div class="flex-row align-items-center">
+                <text class="top-nav-item">热点</text>
+                <text class="top-nav-item">热点</text>
+                <text class="top-nav-item" style="color:#f1a7a9">热点</text>
+                <text class="top-nav-item">热点</text>
+                <text class="top-nav-item">热点</text>
+                <text class="top-nav-item">热点</text>
+                <text class="top-nav-item">热点</text>
+                <text class="top-nav-item">热点</text>
+                <text class="top-nav-item">热点</text>
+                <text class="top-nav-item">热点</text>
+            </div>
+        </scroller>
+        <div class="line"></div>
+
+        <list style="height: 1000px" class="news-list">
+            <cell class="news-cell"  v-for="data in news">
+                <text class="news-item-title">李嘉诚名下尝试集团出售香港中环中，成交金额402港元...</text>
+                <div class="news-item-pic-wrap flex-row">
+                    <image class="news-item-pic"></image>
+                    <image class="news-item-pic"></image>
+                    <image class="news-item-pic"></image>
+                </div>
+                <div class="flex-row mt20 mb30">
+                    <text class="news-item-text">虎嗅网</text>
+                    <text class="news-item-text">11评论</text>
+                    <text class="news-item-text">20分钟前</text>
+                </div>
+                <div class="line"></div>
+            </cell>
+            <cell style="height: 90px;"></cell>
+        </list>
 
         <div class="nav-bar flex-row">
             <div class="nav-bar-item row-item align-items-center" @click="jump('template/a')">
@@ -76,30 +79,47 @@
         background-color: #d53c3f;
         padding: 20px;
         justify-content: space-around;
+        align-items: center;
     }
     .header-title{
-        font-size: 50px;
+        font-size: 46px;
         font-weight: 600;
         color:#fff;
     }
     .header-input{
-        border-radius: 5px;
+        border-radius: 10px;
         padding: 10px;
         background-color: #f6f5f3;
         height: 55px;
         width: 500px;
+        margin-left: 10px;
     }
-    .border-bottom{
-        border-bottom-width:1px;
-        border-bottom-color: #EAEAEA;
-        border-bottom-style: solid;
+
+    .scroller {
+        width: 750px;
+        height: 75px;
+        flex-direction: row;
+        align-items: center;
     }
+    .line{
+        height: 1px;
+        background-color: #EAEAEA;
+    }
+
     .top-nav-item{
-        font-size: 32px;
+        font-size: 30px;
         color:#292929;
-        padding: 25px;
+        padding-left: 20px;
+        padding-right: 20px;
     }
-    .news-item{
+
+    .news-list{
+        align-items: center;
+    }
+    .news-cell{
+        width: 750px;
+        padding-left: 30px;
+        padding-right: 30px;
         padding-top: 36px;
     }
     .news-item-title{
@@ -107,14 +127,20 @@
         font-weight: 600;
     }
     .news-item-pic-wrap{
+        justify-content: space-between;
         margin-top: 15px;
     }
     .news-item-pic{
         width: 225px;
         height: 145px;
-        background-color: #ccc;
-        margin: 10px;
+        background-color: #dfdfdf;
     }
+    .news-item-text{
+        margin-right: 20px;
+        color:#9f9f9f;
+        font-size: 22px;
+    }
+
     .nav-bar{
         position: fixed;
         bottom: 0;
@@ -136,11 +162,6 @@
         font-size: 20px;
         padding-top: 10px;
     }
-    .news-item-append{
-        padding: 20px;
-    }
-    .news-item-append-text{
-        color:#9f9f9f;
-        font-size: 20px;
-    }
+
+
 </style>
