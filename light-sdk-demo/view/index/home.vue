2c4e31f1bc88f8d36660c3b126464b74dbeeb467
Abel <sdwyub@qq.com>
2017-12-05 09:31:44 +0800

    增加demo

---
+++ light-sdk-demo/view/index/home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="title">
+            <code class="more"></code>
+            <span>首 页</span>
+            <code class="scan"></code>
+        </div>
+        <div class="main">
+            <h1>欢迎来到Light！</h1>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {}
+        }
+    }
+</script>
+<style lang="less">
+
+</style>
