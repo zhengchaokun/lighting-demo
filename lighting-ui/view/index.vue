5bac377128a1c1f2de4e5b41f2e33c1553d8b83b
Jervis <jevirs@foxmail.com>
2018-01-08 18:14:37 +0800

    add new files

--- lighting-ui/view/index.vue
+++ lighting-ui/view/index.vue
@@ -1,45 +1,21 @@
 <template>
     <div class="wrapper">
-        <div class="btn yellow  btn-margin" @click="openBottomPopup">
-            <text class="btn-txt">Click</text>
-            <text class="btn-txt" @click="xxxxxxxxx()">Click2</text>
-        </div>
-        <lc-popup popup-color="rgb(92, 184, 92)"
-                   :show="isBottomShow"
-                   @wxcPopupOverlayClicked="popupOverlayBottomClick"
-                   pos="left"
-                   width="500">
-            <div class="demo-content">
-                <image src="https://img.alicdn.com/tfs/TB1ojYvOXXXXXaOXFXXXXXXXXXX-180-41.png" class="demo-image"></image>
-                <text>Different from a "web app", "HTML5 app", or "hybrid app", you can use Weex to build a real mobile app. The code that you write is relatively simple, because you can build native applications just using HTML, CSS, Javascript.
-                </text>
-            </div>
-        </lc-popup>
+        <lc-grid :cols="3" :list="list" :border="true"></lc-grid>
     </div>
 </template>
 
 <script>
     import Light from "light";
-    import LcPopup from 'lighting-ui/packages/lc-popup';
-    console.log(Light)
+    import { LcGrid } from 'lighting-ui';
+    import DATA from 'DATA'
+
     export default {
-        components: { LcPopup },
+        components:{LcGrid},
         data: () => ({
-            isBottomShow: false,
-            height: 400
+            list: DATA
         }),
         methods: {
-            openBottomPopup () {
-                this.isBottomShow = true;
-            },
-            popupOverlayBottomClick () {
-                this.isBottomShow = false;
-            },
-            xxxxxxxxx(){
-                Light.requireModule("modal").alert({
-                    message:"hello"
-                })
-            }
+
         }
     };
 </script>
