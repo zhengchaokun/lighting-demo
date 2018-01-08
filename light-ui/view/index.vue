8db295035733e381ef13c59bf820ffc306ec4ff4
Abel <sdwyub@qq.com>
2018-01-08 10:45:15 +0800

    增加light-ui、 demo

---
+++ light-ui/view/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="wrapper">
+        <div class="btn yellow  btn-margin" @click="openBottomPopup">
+            <text class="btn-txt">Click</text>
+        </div>
+        <lc-popup popup-color="rgb(92, 184, 92)"
+                   :show="isBottomShow"
+                   @wxcPopupOverlayClicked="popupOverlayBottomClick"
+                   pos="left"
+                   width="500">
+            <div class="demo-content">
+                <image src="https://img.alicdn.com/tfs/TB1ojYvOXXXXXaOXFXXXXXXXXXX-180-41.png" class="demo-image"></image>
+                <text>Different from a "web app", "HTML5 app", or "hybrid app", you can use Weex to build a real mobile app. The code that you write is relatively simple, because you can build native applications just using HTML, CSS, Javascript.</text>
+            </div>
+        </lc-popup>
+    </div>
+</template>
+
+<script>
+    import LcPopup from 'light-ui/packages/lc-popup';
+    export default {
+        components: { LcPopup },
+        data: () => ({
+            isBottomShow: false,
+            height: 400
+        }),
+        methods: {
+            openBottomPopup () {
+                this.isBottomShow = true;
+            },
+            popupOverlayBottomClick () {
+                this.isBottomShow = false;
+            }
+        }
+    };
+</script>
