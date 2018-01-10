dff4b5a6ab962fca48a60f1c7882f41ac0856618
Jervis <jevirs@foxmail.com>
2018-01-10 14:48:02 +0800

    update

--- lighting-ui/view/radio.vue
+++ lighting-ui/view/radio.vue
@@ -1,16 +1,65 @@
-
+<!-- CopyRight (C) 2017-2022 Alibaba Group Holding Limited. -->
 <template>
-    <div>
-       Hello,World !
-    </div>
+  <div class="wxc-demo">
+    <scroller class="scroller">
+      <title title="lc-radio"></title>
+      <category title="Radio可选"></category>
+      <lc-radio :list="list"
+                 @LcRadioListChecked="LcRadioListChecked"></lc-radio>
+      <text class="radio-text">checkedItem: {{checkedInfo}}</text>
+      <category title="Radio不可选"></category>
+      <lc-radio :list="list2"></lc-radio>
+    </scroller>
+  </div>
 </template>
-<script>
-    export default {
-        data(){
-            return {}
-        }
-    }
-</script>
-<style lang="less">
 
+<style scoped>
+  .wxc-demo {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background-color: #fff;
+  }
+
+  .scroller {
+    flex: 1;
+  }
+
+  .radio-text {
+    font-size: 30px;
+    color: #333;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    margin-left: 24px;
+  }
 </style>
+
+<script>
+  import Title from '../_mods/title.vue';
+  import Category from '../_mods/category.vue';
+  import LcRadio from 'lighting-ui/packages/lc-radio';
+
+  export default {
+    components: { Title, Category, LcRadio },
+    data: () => ({
+      list: [
+        { title: '选项1', value: 1 },
+        { title: '选项2', value: 2, checked: true },
+        { title: '选项3', value: 3 },
+        { title: '选项4', value: 4 }
+      ],
+      list2: [
+        { title: '未选不可修改', value: 5, disabled: true },
+        { title: '已选不可修改', value: 6, disabled: true, checked: true }
+      ],
+      checkedInfo: { title: '选项2', value: 2, oldIndex: -1, index: 1 }
+    }),
+    methods: {
+      LcRadioListChecked (e) {
+        this.checkedInfo = e;
+      }
+    }
+  }
+</script>
--- lighting-ui/view/result.vue
+++ lighting-ui/view/result.vue
@@ -1,16 +1,50 @@
+<!-- CopyRight (C) 2017-2022 Alibaba Group Holding Limited. -->
+<!-- Created by Tw93 on 16/10/26. -->
 
 <template>
-    <div>
-       Hello,World !
-    </div>
+  <div class="wxc-demo">
+    <lc-result  :icon="icon"
+                :title="title"
+                :desc="desc"
+                padding-top="232"
+                @onResultClick="resultClicked"
+                ></lc-result>
+  </div>
 </template>
-<script>
-    export default {
-        data(){
-            return {}
-        }
-    }
-</script>
-<style lang="less">
 
+<style scoped>
+  .wxc-demo {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background-color: #fff;
+  }
 </style>
+
+<script>
+  import Light from 'light'
+  const modal = Light.requireModule('modal');
+
+  import LcResult from 'lighting-ui/packages/lc-result';
+
+  import TYPE from 'result.js'
+
+  export default {
+    components: { LcResult },
+    data: () => ({
+      icon: TYPE['error']['icon'],
+      title: TYPE['error']['title'],
+      desc: TYPE['error']['desc'],
+    }),
+    methods: {
+      resultClicked (e) {
+        modal.toast({
+          'message': e.title,
+          'duration': 2
+        })
+      }
+    }
+  };
+</script>
