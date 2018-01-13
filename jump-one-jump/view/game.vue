80e0e28f3450a1d37acd51bd0462c7fd9faf4a70
Abel <sdwyub@qq.com>
2018-01-13 13:28:02 +0800

    使用requireModule

---
+++ jump-one-jump/view/game.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <div>
+       <canvas ref="canvas"></canvas>
+    </div>
+</template>
+<script>
+    const THREE = require("three");
+    export default {
+        data(){
+            return {}
+        },
+        mounted(){
+            const canvas = this.$refs.canvas;
+
+
+        }
+    }
+</script>
+<style lang="less">
+
+</style>
