548ef3477c995872ee73674533b09f480f2b347f
Jervis <jevirs@foxmail.com>
2018-01-15 18:06:04 +0800

    update market.vue

--- lighting-ui-demoApp/view/market.vue
+++ lighting-ui-demoApp/view/market.vue
@@ -1,13 +1,42 @@
 
 <template>
     <div>
-       Hello,World !
+        <lc-tab-page 
+          ref="lc-tab-page"
+          :tab-titles="tabTitles"
+          :tab-styles="tabStyles"
+          title-type="icon"
+          :needSlider="needSlider"
+          :is-tab-view="isTabView"
+          :tab-page-height="tabPageHeight"
+          @LcTabPageCurrentTabSelected="LcTabPageCurrentTabSelected">
+          <list v-for="(item,index) in tabList"
+            :key="index">
+          </list>
+        </lc-tab-page>
     </div>
 </template>
 <script>
+    import Config from 'config/market.js';
+    import LcTabPage from 'lighting-ui/packages/lc-tab-page';
+
     export default {
+        components: { LcTabPage },
         data(){
-            return {}
+            return {
+                tabTitles: Config.tabTitles,
+                tabStyles: Config.tabStyles,
+                tabList: [],
+                needSlider: true,
+                supportSlide: true,
+                isTabView: true,
+                tabPageHeight: 380
+            }
+        },
+        methods:{
+            LcTabPageCurrentTabSelected (e) {
+                const self = this;
+            }
         }
     }
 </script>
