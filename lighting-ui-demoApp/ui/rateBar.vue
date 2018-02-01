db56fc61b6c3eadcaa22285bafa3074197d152fc
Jervis <jevirs@foxmail.com>
2018-02-01 11:35:40 +0800

    add progress in quote

---
+++ lighting-ui-demoApp/ui/rateBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+      <div>
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+      rate: {
+          type: Number,
+          default: 20
+      },
+      bgColor: {
+          type: String,
+          default: '#fbfbfb'
+      },
+      barColor: {
+          type: String,
+          default: '#eaeaea'
+      }
+  }
+}
+</script>
+
--- lighting-ui-demoApp/view/share.vue
+++ lighting-ui-demoApp/view/share.vue
@@ -1,13 +1,37 @@
 
 <template>
     <div>
-       Hello,World !
+       <lc-input type="text" value="value1" v-if="!isPwd"></lc-input>
+       <lc-input type="password" value="value2" v-if="isPwd"></lc-input>
+       <div @click="show()">显示密码</div>
     </div>
 </template>
 <script>
+    import LcInput  from 'lighting-ui/packages/lc-input';
     export default {
+        components:{LcInput},
         data(){
-            return {}
+            return {
+                value1: '',
+                value2: '',
+                isPwd: true
+            }
+        },
+        methods:{
+            show(){
+                let tmp;
+                if(this.isPwd){
+                    tmp = this.value2;
+                    console.log(tmp)
+                    // this.isPwd = !this.isPwd;
+                    // this.value1 = tmp;  
+                }else{
+                    tmp = this.value1;
+                    console.log(tmp)
+                    this.isPwd = !this.isPwd;
+                    this.value2 = tmp;
+                }
+            }
         }
     }
 </script>
