996a126257cf645badcf5120d652e3af0be0b283
Abel <sdwyub@qq.com>
2018-02-06 09:18:58 +0800

    增加密码框切换demo

---
+++ lighting-ui/view/togglepass.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <div>
+        <input type="text" @change="change" v-if="type=='text'" v-model="text1"/>
+        <input type="password" @change="change" v-if="type=='password'" v-model="text2"/>
+        <button @click="toggle">切换</button>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                text1:'',
+                text2:'',
+                type:"password"
+            }
+        },
+        methods:{
+            toggle(){
+                const that =this;
+                if(this.type==='password'){
+                    this.type= "text";
+                    this.$nextTick(function () {
+                        that.text1 = that.text2;
+                    })
+                }else{
+                    this.type= "password";
+                    this.$nextTick(function () {
+                        that.text2 = that.text1;
+                    })
+                }
+            },
+            //必须有这个
+            change(){}
+        }
+    }
+</script>
+<style scoped>
+
+</style>
