f2e8114a433a650aca095c2f60871626674ec0de
Jervis <jevirs@foxmail.com>
2018-02-27 15:20:14 +0800

    update news-list

--- lighting-ui-demoApp/view/news_list.vue
+++ lighting-ui-demoApp/view/news_list.vue
@@ -1,12 +1,7 @@
 
 <template>
     <div>
-       <div class="title-bar">
-           <image src="images/back.png" class="icon-back" @click="onBackClick()"></image>
-           <text class="fs32 f-white">{{title}}</text>
-           <image src="images/search.png" class="icon-search mr20" @click="onSearchClick()"></image>
-       </div>
-       <scroller>
+       <scroller style="margin-top:80px;">
            <div class="news-item" v-for="news in newsData">
                <text class="news-item-title f-black">{{news.title}}</text>
                <div class="row-item align-end">
@@ -14,6 +9,13 @@
                </div>
            </div>
        </scroller>
+
+        <!-- 顶部导航栏 -->
+       <div class="title-bar">
+           <image src="images/back.png" class="icon-back" @click="onBackClick()"></image>
+           <text class="fs32 f-white">{{title}}</text>
+           <image src="images/search.png" class="icon-search mr20" @click="onSearchClick()"></image>
+       </div>
     </div>
 </template>
 <script>
@@ -33,7 +35,26 @@
                 },{
                     title: '新华百货（600785）2018-02-26 09:30至2018-02-27 09：30停牌一天。重大事项未公告。',
                     date: '2018-02-26'
-                }]
+                },{
+                    title: '富控互动（600634）2018-02-26 09:30至2018-02-27 09：30停牌一天。重大事项未公告。',
+                    date: '2018-02-26'
+                },{
+                    title: '风范股份（601700）2018-02-26 09:30至2018-02-27 09：30停牌一天。重大事项未公告。',
+                    date: '2018-02-26'
+                },{
+                    title: '风范股份（601700）2018-02-26 09:30至2018-02-27 09：30停牌一天。重大事项未公告。',
+                    date: '2018-02-26'
+                },{
+                    title: '新华百货（600785）2018-02-26 09:30至2018-02-27 09：30停牌一天。重大事项未公告。',
+                    date: '2018-02-26'
+                },{
+                    title: '富控互动（600634）2018-02-26 09:30至2018-02-27 09：30停牌一天。重大事项未公告。',
+                    date: '2018-02-26'
+                },{
+                    title: '风范股份（601700）2018-02-26 09:30至2018-02-27 09：30停牌一天。重大事项未公告。',
+                    date: '2018-02-26'
+                }
+                ]
             }
         },
         methods:{
@@ -49,6 +70,9 @@
 <style scoped src="../css/ui.css"></style>
 <style lang="less">
     .title-bar{
+        position: fixed;
+        left: 0;
+        right: 0;
         height: 80px;
         background-color: #df2f2f;
         flex-direction: row;
