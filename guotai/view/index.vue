a52fb93ee4c7adc2a7d0a93d8c7dbec86a46325e
ruanzhen18683 <253840533@qq.com>
2018-02-27 16:44:35 +0800

    guotai

---
+++ guotai/view/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div style="">
+        <text>hello,world</text>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {};
+        }
+    }
+</script>
+<style scoped>
+</style>
