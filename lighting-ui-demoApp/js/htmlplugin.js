e5ddd149eeaa590d10da952eecaaa67f0f0eb830
曾大鹏 <zengdp11184@hundsun.com>
2018-03-01 13:10:59 +0800

    1.使用canvas组件完成沪深页面头部指数分时图

---
+++ lighting-ui-demoApp/js/htmlplugin.js
@@ -0,0 +1,45 @@
+var context;
+
+function getProto(Weex) {
+  return {
+    create: function() {
+      this.node = document.createElement('canvas')
+      return this.node
+    },
+    getContext: function(type) {
+      context = this.node.getContext(type || '2d')
+      return context
+    },
+
+    getContextAsyn: function(type, callback) {
+      context = this.node.getContext(type || '2d')
+      callback(context)
+    },
+
+    drawImage: function(url) {
+      var img = new Image();
+      img.src = url
+      arguments[0] = img
+      return context.drawImage.apply(context, arguments)
+    }
+
+  }
+}
+
+function init(Weex) {
+  const Component = Weex.Component
+  const extend = Weex.utils.extend
+
+  function GCanvas(data) {
+    Component.call(this, data)
+  }
+
+  GCanvas.prototype = Object.create(Component.prototype)
+  extend(GCanvas.prototype, getProto(Weex))
+
+  Weex.registerComponent('gcanvas', GCanvas)
+}
+
+module.exports = {
+  init: init
+}
