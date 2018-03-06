43334f3e1477c8e762fb51fba8cb7026da7f70d4
Abel <sdwyub@qq.com>
2018-03-06 13:40:00 +0800

    增加新项目

---
+++ ZT-H5/view/lay/inner/bs.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div>
+       Hello,World !
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {}
+        }
+    }
+</script>
+<style lang="less">
+
+</style>
