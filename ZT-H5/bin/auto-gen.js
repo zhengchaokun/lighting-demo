572ed5055fd6ff93bfdf3cf498333f6ba2e0fdf4
Abel <sdwyub@qq.com>
2018-03-12 13:40:31 +0800

    提交接口

--- ZT-H5/bin/auto-gen.js
+++ ZT-H5/bin/auto-gen.js
@@ -10,8 +10,28 @@ for(let key in paths){
         ${paths[key][methods].operationId}(params){
             return execute('${methods}','${key}',params);
         },
-        `
+        `;
+
+        try {
+            let data = paths[key][methods].responses['200'].schema.allOf[1].properties.data;
+            if(data.$ref){
+                data = require("./base").definitions[data.$ref.replace('#/definitions/','')]
+                let map = data.properties
+                let write = {}
+                Object.keys(map).forEach(function (ink) {
+                    write[ink]=""
+                });
+                require("fs").writeFileSync(`../mock${key}.json`,`
+                        {
+                          "code": 0,
+                          "message": "string",
+                          "data": ${JSON.stringify(write)}
+                        }`
+                );
+            }
+        }catch(e){
+            console.log(e)
+        }
     }
 }
 
-console.log(js)
