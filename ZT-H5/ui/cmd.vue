970a59c1555c16bae45f9ec49c51684d0186679d
Abel <sdwyub@qq.com>
2018-03-13 14:25:18 +0800

    功能修改

--- ZT-H5/ui/cmd.vue
+++ ZT-H5/ui/cmd.vue
@@ -2,30 +2,32 @@
 <template>
     <div>
        <div class="operateWrap">
-           <div v-if="com.checkProd" class="buyCont buyStyle">
+           <div v-if="com.prodData" class="buyCont buyStyle">
                 <div class="itemList">
                     <div class="subitem">
-                        <span>产品</span><span>301杭实善成</span><em>选择</em>
+                        <span>产品</span><span>{{com.prodData.fundList[com.prodData.current].fundCode}}{{com.prodData.fundList[com.prodData.current].fundName}}</span><em>选择</em>
                     </div>
                 </div>
             </div>
-            <div class="codeMenu flex"  v-if="com.checkFilter">
-                <span class="flex1" :class="{current:currentFilterIdx==index}" v-for="(filter,index) in com.checkFilter">{{filter.title}}</span>
-            </div>
-            <div class="operateList" v-for="(item,index) in list" @click="currentItemChecked=index">
-                <span class="operateBut" :class="com.clickBtn.cls" v-show="currentItemChecked==index"><em>{{com.clickBtn.title}}</em></span>
-                <h2>30101 焦煤投资策略组合1/产品名称</h2>
-                <p>
-                    <span class="code">{{item.code}}</span>
-                    <span class="colorRed">{{item.status}}</span>
-                    <span>{{item.type}}</span>
-                    <span>持仓均价<em>4448.0</em></span>
-                </p>
-                <p>
-                    <span>指令数量<em>12</em></span>
-                    <span>已委托<em>12</em></span>
-                    <span>已成交<em>12</em></span>
-                </p>
+            <div v-if="com.listData">
+                <div class="codeMenu flex" v-if="currentFilter!=='default'">
+                    <span class="flex1" :class="{current:currentFilter==key}" @click="currentFilter=key" v-for="(item,key) in com.listData">{{key}}</span>
+                </div>
+                <div class="operateList" v-for="(item,index) in com.listData[currentFilter]" @click="currentItemChecked=index">
+                    <span class="operateBut" :class="com.clickBtn.cls" @click="com.clickBtn.handler(item)" v-show="currentItemChecked==index"><em>{{com.clickBtn.title}}</em></span>
+                    <h2>{{item.fundId}} {{item.combiName}}/{{item.fundName}}</h2>
+                    <p>
+                        <span class="code">{{item.insId}}</span>
+                        <span class="colorRed">{{dictByValue('entrustDirection',item.entrustDirection).substring(0,2)}}</span>
+                        <span>{{dictByValue('entrustDirection',item.entrustDirection).substring(2,4)}}</span>
+                        <span>持仓均价<em>{{item.insPrice}}</em></span>
+                    </p>
+                    <p>
+                        <span>指令数量<em>{{item.entrustAmount}}</em></span>
+                        <span>已委托<em>{{item.insAmount}}</em></span>
+                        <span>已成交<em>{{item.dealAmount}}</em></span>
+                    </p>
+                </div>
             </div>
        </div>
     </div>
@@ -34,33 +36,25 @@
     export default {
         data(){
             return {
-                currentFilterIdx:0,
-
-                list:[{
-                    code:'IF1806',
-                    status:'买入',
-                    type:"投机"
-                },{
-                    code:'IF1806',
-                    status:'买入',
-                    type:"投机"
-                },{
-                    code:'IF1806',
-                    status:'买入',
-                    type:"投机"
-                }],
-
+                currentFilter:'default',
                 currentItemChecked:null
             }
         },
         methods:{
             itemClicked(item){
-
             }
         },
         mounted(){
         },
-        props:['com']
+        props:['com'],
+        watch:{
+            'com.listData':function(){
+                if(this.com.listData){
+                    this.currentItemChecked = null;
+                    this.currentFilter = Object.keys(this.com.listData)[0]
+                }
+            }
+        }
     }
 </script>
 <style lang="less">
