cc044f6f06719528b31936433b3cda33d3e9bf60
Abel <sdwyub@qq.com>
2018-04-09 09:56:20 +0800

    增加图标

---
+++ light-error/view/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div style="">
+        <text>hello</text>
+        <text v-if="mgs===1">hello1</text>
+        <text v-if="mgs===3">hello3</text>
+        <text v-if="mgs===2">hello2</text>
+        <text v-if="mgs===4">hello4</text>
+    </div>
+</template>
+<script>
+    import Light from "light";
+    export default {
+        data(){
+            return {
+                mgs:null
+            };
+        },
+        mounted(){
+            const that = this;
+            Light.on("error",function (type) {
+                that.mgs = type
+            })
+        }
+    }
+</script>
+<style scoped>
+</style>
