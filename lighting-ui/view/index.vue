fa8d72d740f74773b2062929ef6cba6a076a168c
ruanzhen18683 <253840533@qq.com>
2018-04-13 11:19:45 +0800

    index

--- lighting-ui/view/index.vue
+++ lighting-ui/view/index.vue
@@ -4,12 +4,14 @@
             <!-- <div v-for="(data,i) in datas" :key="i">
                 <lc-cell :label="data.label" :link="data.path"></lc-cell>
             </div> -->
-            <lc-cell :cell-style="style" :has-bottom-border="hasBottomBorder(i)" :has-top-border="hasTopBorder(i)" v-for="(data,i) in datas" :key="i" :label="data.label" :link="data.path"></lc-cell>
+            <lc-cell :cell-style="style" :has-bottom-border="hasBottomBorder(i)" :has-top-border="hasTopBorder(i)" 
+            v-for="(data,i) in datas" :key="i" :label="data.label" @LcCellClicked="LcCellClicked(data.path,data.label)"></lc-cell>
         </div>
     </scroller>
 </template>
 
 <script>
+    import Light from 'light';
     import LcCell from 'lighting-ui/packages/lc-cell';
     import datas from '../lib/DATA.js';
     
@@ -26,6 +28,9 @@
             },
              hasBottomBorder(i) {
                 return i== this.datas.length-1 ? false : true;
+            },
+            LcCellClicked(src,title){
+                Light.navigate(src, {}, {title: title})
             }
 
         }
