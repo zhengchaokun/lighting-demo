cd360c7b3d435785308accd2801ffc0d7569ae2c
ruanzhen18683 <253840533@qq.com>
2018-04-23 14:01:56 +0800

    hushen

--- lighting-ui-demoApp/view/market/hushen.vue
+++ lighting-ui-demoApp/view/market/hushen.vue
@@ -578,12 +578,12 @@
                 if (index == this.btnIndex) {
                     return {
                     color: "#fe6a6b",
-                    "font-size": "32px"
+                    "font-size": "16px"
                     };
                 } else {
                     return {
                     color: "#9a9a9a",
-                    "font-size": "30px"
+                    "font-size": "15px"
                     };
                 }
             },
