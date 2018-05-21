06049cd540c53b9b9a4b9b0dd3a022459bb4e190
unknown <chenljy19172@hs.handsome.com.cn>
2018-05-21 16:39:02 +0800

    工程目录

--- lightui-desktop/view/index/dialog.vue
+++ lightui-desktop/view/index/dialog.vue
@@ -1,13 +1,49 @@
 
 <template>
     <div>
-       Hello,World !
+        <div class="sub-sidebar">
+            <div class="sub-sidebar-title">{{ currentMenu.title }}</div>
+            <ul class="sub-sidebar-menu">
+                <template  v-for="(item,index) in currentMenu.options">
+                    <li class="sub-sidebar-li" @click="selectOption(item)" :class="{'selected':currentPath.indexOf(item.path) > -1}" :key="index">
+                        {{ item.title }}
+                    </li>
+                </template>
+            </ul>
+        </div>
+        <div class="content">
+            <router-view></router-view>
+        </div>  
     </div>
 </template>
 <script>
+    import menus from "menus"
+    import App from "light"
+    // import API from 'api'
     export default {
         data(){
-            return {}
+            return {
+                currentMenu: menus[3],
+                currentPath: '',
+                navList:['弹框页','基础弹框']
+            }
+        },
+        methods:{
+            selectOption(item){
+                App.navigate('#' + item.path,{});
+                this.currentPath = item.path;
+            }
+        },
+        watch:{
+            "$route":function(val){
+                this.currentPath = this.$route.path;
+            }
+        },
+        created(){
+            App.trigger('navChange', '')
+        },
+        mounted(){
+            this.currentPath = this.$route.path;
         }
     }
 </script>
